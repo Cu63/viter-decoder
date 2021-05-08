de66aaff929c1a03ad98299684307983ceadabd5
Cu63 <scube.str@gmail.com>
2021-05-08 14:21:15 +0300

    Change function nextIndexAndState

--- src/viter.cpp
+++ src/viter.cpp
@@ -1,5 +1,6 @@
 #include "../header/viter.hpp"
 #include <iostream>
+#include <iomanip>
 
 
 Viter::Viter(int k) {
@@ -11,6 +12,8 @@ Viter::Viter(int k) {
     codeIn.push_back(0);
     codeIn.push_back(0);
     signalIn.resize(codeIn.size());
+    codeOut.resize(codeIn.size());
+    signalOut.resize(signalIn.size());
 }
 
 void Viter::readSignal() {
@@ -51,6 +54,21 @@ short Viter::encode(short a) {
     return tmp;
 }
 
+short Viter::findMin(codeMatrix &s) {
+    short min;
+    short index;
+
+    min = 127;
+    index = -1;
+    for (int i = 0; i < k; ++i) {
+        if (s[i][s[0].size() - 1] < min) {
+            min = s[i][s[0].size() - 1];
+            index = i;
+        }
+    }
+    return index;
+}
+
 void Viter::decode() {
     codeMatrix state(k, std::vector<short> (codeIn.size()));
     codeMatrix index(k, std::vector<short> (codeIn.size()));
@@ -61,51 +79,69 @@ void Viter::decode() {
         index[i][0] = 0;
     }
 
-
-    std::cout << state[0].size() << " calc\n";
     for (int i = 1; i < state[0].size(); ++i) {
         for (int j = 0; j < k; ++j) {
+            /*
             short ind;
             ind = setIndex(state, index, i, j);
             index[j][i] = ind;
             state[j][i] = state[ind][i - 1]
                 + calcHammingDistance(A[j][ind], codeIn[i]);
+                */
+            nextIndexAndState(state, index, i, j);
         }
+    }
+
+    codeOut[codeOut.size() - 1] = findMin(state);
+
+    for (int i = codeOut.size() - 1; i > 0; --i) {
+        codeOut[i - 1] = index[codeOut[i]][i];
     }
 
     std::cout << "Index:\n";
     for (int i = 0; i < k; ++i) {
         for (int j = 0; j < state[i].size(); ++j)
-            std::cout << index[i][j] << " ";
+            std::cout << std::setw(2) << index[i][j] << " ";
         std::cout << std::endl;
     }
 
     std::cout << "\nState:\n";
     for (int i = 0; i < k; ++i) {
         for (int j = 0; j < state[i].size(); ++j)
-            std::cout << state[i][j] << " ";
+            std::cout << std::setw(2) << state[i][j] << " ";
         std::cout << std::endl;
     }
 
+    for (int i = 0; i < codeOut.size(); ++i)
+        std::cout << codeOut[i] << " ";
+    std::cout << std::endl;
 }
 
-short Viter::setIndex(codeMatrix &s, codeMatrix &ind, int x, int y) {
+void Viter::nextIndexAndState(codeMatrix &s, codeMatrix &ind, int x, int y) {
     short index;
     short min;
 
     index = -1;
     min = 127;
     for (int i = 0; i < k; ++i) {
+        /*
+         std::cout << "From " << i << " to " << y
+            << " distance is " << calcHammingDistance(A[y][i], codeIn[x]);
+            */
         short tmp;
         if (A[y][i] != -1 && s[i][x - 1] != -1) {
-            tmp = s[i][x - 1] + calcHammingDistance(A[i][y], codeIn[x]);
-            if (tmp < min) {
-                min = tmp;
-                index = i;
-            }
+           tmp = s[i][x - 1] + calcHammingDistance(A[y][i], codeIn[x]);
+           if (tmp < min) {
+               min = tmp;
+               index = i;
+           }
+//           std::cout << ", summary distance is " << tmp;
         }
+ //       std::cout << std::endl;
     }
-    return index;
+  //  std::cout << "\n";
+    ind[y][x] = index;
+    s[y][x] = min;
 }
 
 /*
